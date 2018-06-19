06ab57fb616d15e6630fc352d76156b9b6d0d2c8
Thanapon Archint <thanapon.archint@gmail.com>
2018-06-19 14:18:37 +0700

    register hover

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
+  
+  ngOnInit(){}
 
-  ngOnInit() {
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }

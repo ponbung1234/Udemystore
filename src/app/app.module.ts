060cb842c4ec1a6a6683d7f34979edf8672d511b
Sukan Sittibamrungsuk <33932065+nai3ertron@users.noreply.github.com>
2018-06-19 13:49:56 +0700

    modal-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -9,6 +10,7 @@ import { ItemComponent } from './item/item.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './/login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ModalRegistComponent } from './modal-regist/modal-regist.component';
 
 
 
@@ -19,12 +21,14 @@ import { RegisterComponent } from './register/register.component';
     AdvertiseComponent,
     ItemComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ModalRegistComponent
   ],
   imports: [
     BrowserModule,
     AngularFontAwesomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal-regist/modal-regist.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-regist',
+  templateUrl: './modal-regist.component.html',
+})
+export class ModalRegistComponent implements OnInit {
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+  
+  ngOnInit(){}
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}

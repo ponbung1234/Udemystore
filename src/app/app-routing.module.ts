ce8e007b6f96a834768cc0edd85699215ff2f788
Thanapon Archint <thanapon.archint@gmail.com>
2018-06-20 15:23:18 +0700

    add orderlist and route to orderlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ItemComponent } from './item/item.component';
 import { RegisterComponent } from './register/register.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
 const routes: Routes = [
  // {path: '', component: ItemComponent},
   {path: '', component: ItemComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'orderlist', component: OrderlistComponent}
   
 ];
 

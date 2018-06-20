e383ce3b011e3c096c83297d26c2517fc1940f73
Thanapon Archint <thanapon.archint@gmail.com>
2018-06-20 08:52:24 +0700

    implement login function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { LoginComponent } from './../login/login.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [LoginComponent]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private comp: LoginComponent) {
+      }
 
   ngOnInit() {
   }
-
+  callMethod(content){
+    //this.loginComponent.open(content);
+    //this.massageEvent.emit(content)
+    this.comp.openVerticallyCentered(content);
+  }
+  
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
@@ -11,23 +12,12 @@ export class LoginComponent implements OnInit {
 
   constructor(private modalService: NgbModal) {}
   
-  ngOnInit(){}
-
-  open(content) {
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+  ngOnInit(){
+    //console.log(this.content);
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
+  openVerticallyCentered(content) {
+    console.log(content);
+    this.modalService.open(content, { centered: true });
   }
 }

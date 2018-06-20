9d147f7426aaaec0eadca8fb3a241c37c25d71f8
Thanapon Archint <thanapon.archint@gmail.com>
2018-06-20 15:45:52 +0700

    .ts orderlist file

---
+++ src/app/orderlist/orderlist.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-orderlist',
+  templateUrl: './orderlist.component.html',
+  styleUrls: ['./orderlist.component.css']
+})
+export class OrderlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { 
+  }
+
+}

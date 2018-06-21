a0e09aa2e858c4885ca2798539a26ac25b11a232
Sukan Sittibamrungsuk <33932065+nai3ertron@users.noreply.github.com>
2018-06-21 17:05:05 +0700

    update-wait-for-fix

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderServiceService } from './../service/header-service.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
  
-  constructor() { }
+  constructor(public nav: HeaderServiceService) { }
 
 
   ngOnInit() {
+    this.nav.show();
   }
 
 }

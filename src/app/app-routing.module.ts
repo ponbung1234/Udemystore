a1162b69ed418c777ea3fdba64d825da86eeb684
Thanapon Archint <thanapon.archint@gmail.com>
2018-06-21 10:55:50 +0700

    add cover page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { ItemComponent } from './item/item.component';
 import { RegisterComponent } from './register/register.component';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import { CoverpageComponent } from './coverpage/coverpage.component';
 const routes: Routes = [
  // {path: '', component: ItemComponent},
   {path: '', component: ItemComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'orderlist', component: OrderlistComponent}
+  {path: 'orderlist', component: OrderlistComponent},
+  {path: 'cover', component: CoverpageComponent}
   
 ];
 

047f3edd028428ce663b968459c2cc74753685d6
Sukan Sittibamrungsuk <33932065+nai3ertron@users.noreply.github.com>
2018-06-27 13:02:50 +0700

    update

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import {Observable} from 'rxjs/observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import 'rxjs/add/observable/throw'

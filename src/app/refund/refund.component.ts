af3cd1e6460335dc55a8730ee918077ac6b263df
Thanapon Archint <thanapon.archint@gmail.com>
2018-07-10 12:57:54 +0700

    fix refund page and add some link

--- src/app/refund/refund.component.ts
+++ src/app/refund/refund.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderServiceService } from './../service/header-service.service';
 import { ActivatedRoute } from '@angular/router';
+import { Refund } from './../refund';
+import { RefundService } from './../service/refund.service';
+import {NgbPopoverConfig} from '@ng-bootstrap/ng-bootstrap';
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-refund',
   templateUrl: './refund.component.html',
@@ -11,21 +15,45 @@ export class RefundComponent implements OnInit {
   closeResult: string;
   numbers: number[];
   amount: number;
+  itemID: any;
+  refund: Refund[] = [];
+  filteredProducts: Refund[] = [];
 
   constructor(
     public nav: HeaderServiceService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private _refundService: RefundService,
+    private route: ActivatedRoute
   ) {
+
+    _refundService
+      .getRefund().switchMap(refund => {
+
+        this.refund = refund;
+        //console.log(this.refund);
+        return route.queryParamMap;
+
+      })
+      .subscribe(params => {
+        this.itemID = params.get('itemID');
+        //console.log(this.itemID);
+        this.filteredProducts = (this.refund) ?
+          this.refund.filter(p => p.item_id === Number(this.itemID)) :
+          this.refund;
+        console.log(this.filteredProducts);
+
+      });
+     // console.log(this.filteredProducts);
+      
 
   }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
+      //this.itemID = params['itemID'];
       this.amount = params['amount'];
-      let itemID = params['itemID'];
       console.log(this.amount);
-      console.log(itemID);
-      this.numbers = Array(10).fill(0).map((x,i)=>i);
+      this.numbers = Array(10).fill(0).map((x, i) => i);
     });
     this.nav.show();
   }

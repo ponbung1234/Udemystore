8b6f2a13a2de608e2784cdc6cf93a729fc6197db
Sukan Sittibamrungsuk <33932065+nai3ertron@users.noreply.github.com>
2018-07-16 17:14:55 +0700

    detail page

--- src/app/detailpage/detailpage.component.ts
+++ src/app/detailpage/detailpage.component.ts
@@ -1,43 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Injectable } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { Products } from '../products';
 import { HeaderServiceService } from '../service/header-service.service';
+import { CartService } from '../service/cart.service'
 
+@Injectable()
 @Component({
   selector: 'app-detailpage',
   templateUrl: './detailpage.component.html',
-  styleUrls: ['./detailpage.component.css']
+  styleUrls: ['./detailpage.component.css'],
+  providers:[CartService]
 })
 export class DetailpageComponent implements OnInit {
-  getproductID;
+  @Output() cartItemAmount: EventEmitter <any> = new EventEmitter<any>();
   productItem: Products[] = [];
-  productFromSearch;
+  productID = 0;
+  productName = '';
+  productDes = '';
+  productPrice = 0;
+  productIMG = '';
+  numberItem = 1;
+  count = 0;
+  cartNumber: number;
+  productSearchID = 12;
+
 
   constructor(
     public nav: HeaderServiceService,
-    private _product: ProductService
+    private _product: ProductService,
+    private _cartService: CartService,
+    
   ) { 
     _product
     .getProducts()
     .subscribe((productItem) => {
-      productItem=productItem;
+      
+      // console.log(productItem);
       if(productItem != null){
-        for(let i = 0 ; i <= productItem.length ; i++ ){
-          this.getproductID = productItem[i].product_id;
-          console.log(this.getproductID)
-          // if(this.productFromSearch == productItem[i].product_id){
-          //   this.productFromSearch = productItem;
-          //   console.log(productItem[i].url);
-          // }
+        for(let i = 0 ; i < productItem.length ; i++ ){
+          if(this.productSearchID == productItem[i].product_id){
+            // console.log(productItem[i])
+            this.productID = productItem[i].product_id;
+            this.productName = productItem[i].product_name;
+            this.productDes = productItem[i].product_description;
+            this.productPrice = productItem[i].price;
+            this.productIMG = productItem[i].url;
+          }
         }
+        // this.productItem = productItem[3].product_id;
       }else {
         alert("no data");
       }
     })
   }
-
+  
   ngOnInit() {
     this.nav.show();
+    this._cartService.cast.subscribe(cartNum => this.cartNumber = cartNum);
+    console.log("detail page num: "+this.cartNumber);
+  }
+  public addItemDet(product: number, numberItem: number){
+    console.log("details:" + product)
+    this._cartService.updateCartNum(product).subscribe((product) => {
+    // console.log("details in :" + product)
+      
+    }, (error) => {
+      console.log(error);
+    });
   }
 
+  plus(){
+    this.count++;
+    this.numberItem = this.count;
+  }
+  minus(){
+    if(this.count > 1 ){
+      this.count--;
+      this.numberItem = this.count;
+    }
+  }
 }
